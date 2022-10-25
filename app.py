1e8b1801fc5990df8305ea94d96dec7678d1917d
zeeshanvirani <zvirani99@gmail.com>
2022-10-25 17:50:17 -0500

    added story generation after modification

--- app.py
+++ app.py
@@ -3,6 +3,13 @@ from bs4 import BeautifulSoup
 
 app = Flask(__name__)
 
+character1_emotion = []
+character2_emotion = []
+character1_type = ""
+character2_type = ""
+willstoryloop = False
+
+
 @app.route("/")
 def index():
     return render_template("index.html")
@@ -10,13 +17,9 @@ def index():
 
 @app.route('/', methods=['POST'])
 def story():
-    character1_emotion = []
-    character2_emotion = []
+    global character1_emotion, character2_emotion, willstoryloop, character1_type, character2_type
     character1_dialogue = []
     character2_dialogue = []
-    character1_type = ""
-    character2_type = ""
-    willstoryloop = False
     xmlFile = request.form.get('xmlFile')
     soup = BeautifulSoup(xmlFile, 'lxml')
     character1_type = soup.find('type_of_character_one').text.strip()
@@ -48,6 +51,56 @@ def story():
                                character2_type=character2_type, character1_dialogue=character1_dialogue,
                                character1_emotion=character1_emotion, character2_dialogue=character2_dialogue,
                                character2_emotion=character2_emotion)
+
+
+@app.route('/generate', methods=['POST'])
+def generateXML():
+    global character1_emotion, character2_emotion, willstoryloop, character1_type, character2_type
+    data = request.get_json()
+    character1_dialogue = data['x']
+    character2_dialogue = data['y']
+    print(character1_dialogue)
+    print(character2_dialogue)
+
+    soup = BeautifulSoup(features='lxml')
+
+    mainStory = soup.new_tag("story")
+    mainStory_Char1Type = soup.new_tag("type_of_character_one")
+    mainStory_Char1Type.string = character1_type
+    mainStory_Char2Type = soup.new_tag("type_of_character_two")
+    mainStory_Char2Type.string = character2_type
+    mainStory_DoesLoop = soup.new_tag("does_story_loop")
+    mainStory_DoesLoop.string = str(willstoryloop)
+    mainStory.append(mainStory_Char1Type)
+    mainStory.append(mainStory_Char2Type)
+    mainStory.append(mainStory_DoesLoop)
+    soup.append(mainStory)
+
+    for i in range(len(character1_emotion)):
+        curScene = soup.new_tag("scene")
+        curScene_char1 = soup.new_tag("characterone")
+        curScene_char2 = soup.new_tag("charactertwo")
+        curScene_char1_emotion = soup.new_tag("emotion")
+        curScene_char1_emotion.string = character1_emotion[i]
+        curScene_char2_emotion = soup.new_tag("emotion")
+        curScene_char2_emotion.string = character2_emotion[i]
+        curScene_char1_dialogue = soup.new_tag("dialogue")
+        curScene_char1_dialogue.string = character1_dialogue[i]
+        curScene_char2_dialogue = soup.new_tag("dialogue")
+        curScene_char2_dialogue.string = character2_dialogue[i]
+
+        curScene_char1.append(curScene_char1_emotion)
+        curScene_char2.append(curScene_char2_emotion)
+        curScene_char1.append(curScene_char1_dialogue)
+        curScene_char2.append(curScene_char2_dialogue)
+        curScene.append(curScene_char1)
+        curScene.append(curScene_char2)
+        soup.append(curScene)
+
+    with open("story.xml", "w") as file:
+        file.write(soup.prettify())
+
+    return send_file("story.xml", mimetype="text/xml", as_attachment=True, download_name="story.xml")
 
 
 if __name__ == "__main__":
