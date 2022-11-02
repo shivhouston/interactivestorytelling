63d7affe2076234cb2877ef524e81d2048e180a7
shivhouston <95387363+shivhouston@users.noreply.github.com>
2022-11-01 20:24:59 -0500

    small bug fix

--- app.py
+++ app.py
@@ -12,6 +12,12 @@ willstoryloop = False
 
 @app.route("/")
 def index():
+    global character1_emotion, character2_emotion, willstoryloop, character1_type, character2_type
+    character1_emotion = []
+    character2_emotion = []
+    character1_type = ""
+    character2_type = ""
+    willstoryloop = False
     return render_template("index.html")
 
 @app.route("/upload-image", methods=['GET', 'POST'])
@@ -53,47 +59,6 @@ def fileupload():
                                        character2_type=character2_type, character1_dialogue=character1_dialogue,
                                        character1_emotion=character1_emotion, character2_dialogue=character2_dialogue,
                                        character2_emotion=character2_emotion)
-
-
-
-
-@app.route('/', methods=['POST'])
-def story():
-    global character1_emotion, character2_emotion, willstoryloop, character1_type, character2_type
-    character1_dialogue = []
-    character2_dialogue = []
-    xmlFile = request.form.get('xmlFile')
-    soup = BeautifulSoup(xmlFile, 'lxml')
-    character1_type = soup.find('type_of_character_one').text.strip()
-    character2_type = soup.find('type_of_character_two').text.strip()
-    willstoryloop = eval(soup.find('does_story_loop').text.strip())
-    scenes = soup.find_all('scene')
-    print(len(scenes))
-    for scene in scenes:
-        emotions = scene.find_all('emotion')
-        character1_emotion.append(emotions[0].get_text()[5:-4])
-        character2_emotion.append(emotions[1].get_text()[5:-4])
-        dialogues = scene.find_all('dialogue')
-        character1_dialogue.append(dialogues[0].get_text()[5:-4])
-        character2_dialogue.append(dialogues[1].get_text()[5:-4])
-    print(willstoryloop)
-    print(character1_type)
-    print(character2_type)
-    print(character1_emotion)
-    print(character1_dialogue)
-    print(character2_emotion)
-    print(character2_dialogue)
-    if request.form['action'] == 'View Story':
-        return render_template("story.html", willstoryloop=willstoryloop, character1_type=character1_type,
-                               character2_type=character2_type, character1_dialogue=character1_dialogue,
-                               character1_emotion=character1_emotion, character2_dialogue=character2_dialogue,
-                               character2_emotion=character2_emotion)
-    elif request.form['action'] == 'Edit Story':
-        return render_template("storyediting.html", willstoryloop=willstoryloop, character1_type=character1_type,
-                               character2_type=character2_type, character1_dialogue=character1_dialogue,
-                               character1_emotion=character1_emotion, character2_dialogue=character2_dialogue,
-                               character2_emotion=character2_emotion)
-
 
 @app.route('/generate', methods=['POST'])
 def generateXML():
